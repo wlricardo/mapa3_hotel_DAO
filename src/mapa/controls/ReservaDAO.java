a43a7a46aef7c4a9c4c2f94eb5174769ccf5c0cb
wlricardo <wlricardo@gmail.com>
2022-03-06 16:53:29 -0300

    Finalizando DAOs

--- src/mapa/controls/ReservaDAO.java
+++ src/mapa/controls/ReservaDAO.java
@@ -1,6 +1,7 @@
 package mapa.controls;
 
 import java.util.List;
+import mapa.entities.Hospede;
 import mapa.entities.Reserva;
 import mapa.entities.Suite;
 import mapa.exceptions.ElementoNaoEncontradoException;
@@ -12,10 +13,11 @@ public interface ReservaDAO {
 
     void verificarCapacidade(Reserva reserva, int capacidade) throws ReservaInvalidaException;
 
-    void adicionarReserva(List<Reserva> lista);
-    
-    Suite procurarSuite(List<Reserva> lista, Suite suite) throws ElementoNaoEncontradoException;
-    
-    void mostrarReserva(List<Reserva> lista, Suite suite) throws ElementoNaoEncontradoException;
+    void adicionarReserva(List<Reserva> lista, Reserva reserva);
 
+    boolean verificarReserva(List<Reserva> lista, Suite suite) throws ElementoNaoEncontradoException;
+
+    List<Hospede> mostrarHospedeSuite(List<Reserva> reserva, Suite suite) throws ElementoNaoEncontradoException;
+
+    void mostrarReserva(List<Reserva> lista, Suite suite) throws ElementoNaoEncontradoException;
 }
--- src/mapa/controls/ReservaImpl.java
+++ src/mapa/controls/ReservaImpl.java
@@ -1,9 +1,11 @@
 package mapa.controls;
 
 import java.util.List;
+import mapa.entities.Hospede;
 import mapa.entities.Reserva;
 import mapa.entities.Suite;
 import mapa.exceptions.ElementoNaoEncontradoException;
+import mapa.exceptions.ReservaInvalidaException;
 
 public class ReservaImpl implements ReservaDAO {
 
@@ -21,24 +23,69 @@ public class ReservaImpl implements ReservaDAO {
             throw new ElementoNaoEncontradoException(msgErro());
         }
     }
-    
+
     @Override
-    public verificarCapacidade(Reserva reserva, int capacidade){
-        for (Hospede h : reserva.getHospedes()) {
-            if (r.getSuite().getCapacidade() >= capacidade) {
-                
-            }
+    public void verificarCapacidade(Reserva reserva, int capacidade) {
+        if (hospedesAcimaDoisAnos(reserva) > capacidade) {
+            throw new ReservaInvalidaException("\n ** Erro! Total de hóspedes maior que a capaciade da suite **\n");
         }
-        
-        
     }
-    
-    public int hospedesAcimaDoisAnos(List<Reserva> lista){
+
+    @Override
+    public void adicionarReserva(List<Reserva> lista, Reserva reserva) {
+        lista.add(reserva);
+    }
+
+    @Override
+    public boolean verificarReserva(List<Reserva> lista, Suite suite) {
+        boolean achou = false;
         for (Reserva r : lista) {
-            if (r.g) {
-                
+            if (r.getSuite().getNumero().equals(suite.getNumero())) {
+                achou = true;
             }
         }
+        return achou;
+    }
+
+    @Override
+    public List<Hospede> mostrarHospedeSuite(List<Reserva> lista, Suite suite) {
+        List<Hospede> hospedes = null;
+        if (verificarReserva(lista, suite)) {
+            for (Reserva r : lista) {
+                if (r.getSuite().equals(suite)) {
+                    hospedes = r.getHospedes();
+                }
+            }
+        } else {
+            throw new ElementoNaoEncontradoException(msgErro());
+        }
+        return hospedes;
+    }
+
+    @Override
+    public void mostrarReserva(List<Reserva> lista, Suite suite) {
+        List<Hospede> h = null;
+        if (verificarReserva(lista, suite)) {
+            System.out.println("Dados da reserva:");
+            System.out.println("----------------");
+            System.out.println(suite.toString());
+
+            h = mostrarHospedeSuite(lista, suite);
+            for (Hospede hospede : h) {
+                System.out.println(hospede.toString());
+            }
+
+        }
+    }
+
+    public int hospedesAcimaDoisAnos(Reserva reserva) {
+        int total = 0;
+        for (Hospede h : reserva.getHospedes()) {
+            if (h.getIdade() > 2) {
+                total++;
+            }
+        }
+        return total;
     }
 
     public String msgErro() {
